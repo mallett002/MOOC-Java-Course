49d3d1e4f237e261b3216b07eae1f3677b883772
William Mallett <MallettWilliam@JohnDeere.com>
2019-05-17 05:43:23 -0500

    adding week 5 exercises

---
+++ src/main/java/week5/StupidLyyraCard/LyyraCard.java
@@ -0,0 +1,30 @@
+package week5.StupidLyyraCard;
+
+public class LyyraCard {
+    private double balance;
+
+    public LyyraCard(double balance) {
+        this.balance = balance;
+    }
+
+    public double balance() {
+        return this.balance;
+    }
+
+    public void loadMoney(double amount) {
+        this.balance += amount;
+    }
+
+    public boolean pay(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+           return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "card balance: " + this.balance;
+    }
+}
