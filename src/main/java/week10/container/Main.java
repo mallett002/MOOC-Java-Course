a24ba3c6a27f693d81ee74ee1a670ba419e10525
William Mallett <mallett002@gmail.com>
2019-09-06 17:46:40 -0500

    Container

--- src/main/java/week10/container/Main.java
+++ src/main/java/week10/container/Main.java
@@ -1,34 +1,10 @@
 package week10.container;
 
-import java.util.Collections;
-
 public class Main {
     public static void main(String[] args) {
-//        ProductContainer juice = new ProductContainer("Juice", 1000.0);
-//        juice.addToTheContainer(1000.0);
-//        juice.takeFromTheContainer(11.3);
-//        System.out.println(juice.getName());
-//        juice.addToTheContainer(1.0);
-//        System.out.println(juice);
-//
-            // Container History:
-//        ContainerHistory history = new ContainerHistory();
-//        history.add(1000);
-//        history.add(11.3);
-//        history.add(1.0);
-//        double max = history.maxValue();
-//        double min = history.minValue();
-//        double average = history.average();
-//        double greatestFluctuation = history.greatestFluctuation();
-//        double variance = history.variance();
-//        System.out.println("max: " + max);
-//        System.out.println("min: " + min);
-//        System.out.println("average: " + average);
-//        System.out.println("greatest fluctuation: " + greatestFluctuation);
-//        System.out.println("variance: " + variance);
-
-
-//        30.7: Product Container Recorder Phase 2
-
+        ProductContainerRecorder juice = new ProductContainerRecorder("Juice", 1000.0, 1000.0);
+        juice.takeFromTheContainer(11.3);
+        juice.addToTheContainer(1.0);
+        juice.printAnalysis();
     }
 }
--- src/main/java/week10/container/ProductContainerRecorder.java
+++ src/main/java/week10/container/ProductContainerRecorder.java
@@ -7,10 +7,34 @@ public class ProductContainerRecorder extends ProductContainer {
         super(productName, capacity); // call ProductContainer constructor with name and capacity
         this.history = new ContainerHistory();
         this.addToTheContainer(initialVolume);
-        this.history.add(initialVolume);
     }
 
     public String history() {
         return this.history.toString();
+    }
+
+    @Override
+    public void addToTheContainer(double amount) {
+        super.addToTheContainer(amount); // add the amount to the container
+        this.history.add(super.getVolume()); // update the history with new volume
+    }
+
+    @Override
+    public double takeFromTheContainer(double amount) {
+        double amountTaken = super.takeFromTheContainer(amount); // take it
+        this.history.add(super.getVolume()); // add volume to history
+        return amountTaken; // return amount taken
+    }
+
+    public void printAnalysis() {
+        System.out.println(
+                "Product: " + super.getName() + "\n" +
+                "History: " + this.history() + "\n" +
+                "Greatest: " + this.history.maxValue() + "\n" +
+                "Smallest: " + this.history.minValue() + "\n" +
+                "Average: " + String.format("%.2f", this.history.average()) + "\n" +
+                "Greatest Change: " + String.format("%.2f", this.history.greatestFluctuation()) + "\n" +
+                "Variance: " + String.format("%.2f", this.history.variance())
+        );
     }
 }
