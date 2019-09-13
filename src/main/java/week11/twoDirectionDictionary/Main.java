32020369696be9a5c9d4d27d22c08bcd686f1cd9
William Mallett <mallett002@gmail.com>
2019-09-12 19:15:05 -0500

    two direction dictionary

--- src/main/java/week11/twoDirectionDictionary/Main.java
+++ src/main/java/week11/twoDirectionDictionary/Main.java
@@ -1,7 +1,16 @@
 package week11.twoDirectionDictionary;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Exercise 35: Two-Direction Dictionary
+    public static void main(String[] args) throws FileNotFoundException {
+        MindfulDictionary dict = new MindfulDictionary("test.txt");
+        dict.load();
+
+        System.out.println( dict.translate("apina") );
+        System.out.println( dict.translate("ohjelmointi") );
+        System.out.println( dict.translate("alla oleva") );
+
+//        Erroring inside translate()
     }
 }
---
+++ src/main/java/week11/twoDirectionDictionary/MindfulDictionary.java
@@ -0,0 +1,81 @@
+package week11.twoDirectionDictionary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MindfulDictionary {
+
+    Map<String, String> dictionary;
+    String file;
+
+    public MindfulDictionary() {
+        this.dictionary = new HashMap<>();
+    }
+
+    public MindfulDictionary(String file) {
+        this.file = file;
+    }
+
+    public boolean load() throws FileNotFoundException {
+        try {
+            File fyle = new File(this.file);
+            Scanner reader = new Scanner(fyle);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] parts = line.split(":");
+                this.dictionary.put(parts[0], parts[1]);
+            }
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void add(String word, String translation) {
+        if (!this.dictionary.containsKey(word) && !this.dictionary.containsValue(translation)) {
+            this.dictionary.put(word, translation);
+        }
+    }
+
+    public void remove(String word) {
+        if (this.dictionary.containsKey(word)) {
+            this.dictionary.remove(word);
+        } else if (this.dictionary.containsValue(word)) {
+            String key = "";
+
+            for (String possibleKey : this.dictionary.keySet()) {
+                if (this.dictionary.get(possibleKey).equals(word)) {
+                    key = possibleKey;
+                }
+            }
+
+            this.dictionary.remove(key);
+        }
+    }
+
+    public String translate(String word) {
+        if (this.dictionary.containsKey(word)) {
+            return this.dictionary.get(word);
+        }
+
+        if (this.dictionary.containsValue(word)) {
+            String key = "";
+
+            for (String possibleKey : this.dictionary.keySet()) {
+                if (this.dictionary.get(possibleKey).equals(word)) {
+                    key = possibleKey;
+                }
+            }
+
+            return key;
+        }
+
+        return null;
+    }
+}
