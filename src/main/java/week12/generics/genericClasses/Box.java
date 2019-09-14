601bfdfd422da4cddfdf471461d1fcf582e74b11
William Mallett <mallett002@gmail.com>
2019-09-14 18:16:19 -0500

    Generics

---
+++ src/main/java/week12/generics/genericClasses/Box.java
@@ -0,0 +1,28 @@
+package week12.generics.genericClasses;
+
+// <T> specifies it takes a generic type
+public class Box<T> {
+    private T t;
+
+    public void add(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return this.t;
+    }
+
+    public static void main(String[] args) {
+        // Make Box with <Integer> type
+        Box<Integer> integerBox = new Box<Integer>();
+
+        // Make Box with <String> type
+        Box<String> stringBox = new Box<String>();
+
+        integerBox.add(99);
+        stringBox.add("barbecue chips");
+
+        System.out.printf("Integer Value :%d\n\n", integerBox.get());
+        System.out.printf("String Value :%s\n", stringBox.get());
+    }
+}
