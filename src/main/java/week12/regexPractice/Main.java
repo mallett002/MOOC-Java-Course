79967967c4d23b3e9a2a9ad3b3ff99cdfaa0bef1
William Mallett <mallett002@gmail.com>
2019-09-19 18:17:59 -0500

    regex practice

--- src/main/java/week12/regexPractice/Main.java
+++ src/main/java/week12/regexPractice/Main.java
@@ -1,23 +1,26 @@
 package week12.regexPractice;
 
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
         // Week Days
-//        System.out.print("Give a day abbreviation: ");
-//        boolean isDay = isAWeekDay(reader.nextLine());
-//        printForm(isDay);
+        System.out.print("Give a day abbreviation: ");
+        boolean isDay = isAWeekDay(reader.nextLine());
+        printForm(isDay);
 
         // Vowel Inspection
         System.out.print("Give a string: ");
         String line = reader.nextLine();
         boolean containesOnlyVowels = allVowels(line);
         printForm(containesOnlyVowels);
+
+        // Clock time
+        System.out.print("Give a time: ");
+        boolean isTime = clockTime(reader.nextLine());
+        printForm(isTime);
     }
 
     private static void printForm(boolean isValid) {
@@ -29,14 +32,15 @@ public class Main {
     }
 
     private static boolean isAWeekDay(String str) {
-//        String[] days = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
-//
-//        return Arrays.stream(days)
-//                .anyMatch(str::matches);
         return str.matches("mon|tue|wed|thu|fri|sat|sun");
     }
 
     private static boolean allVowels(String str) {
         return str.matches("[aeiou]*");
+    }
+
+    public static boolean clockTime(String string) {
+        String regex = "([0-1][0-9]|2[0-3])(:[0-5][0-9]){2}";
+        return string.matches(regex);
     }
 }
