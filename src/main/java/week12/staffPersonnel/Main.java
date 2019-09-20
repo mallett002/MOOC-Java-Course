500f3ccf405ab80ec22610906c1b87661e61b20a
William Mallett <mallett002@gmail.com>
2019-09-19 19:12:04 -0500

    Making staffPersonnel

---
+++ src/main/java/week12/staffPersonnel/Main.java
@@ -0,0 +1,10 @@
+package week12.staffPersonnel;
+
+public class Main {
+    public static void main(String[] args) {
+        Person arto = new Person("Arto", Education.DOCTOR);
+        System.out.println(arto);
+
+        // Exercise 45.3: Employees
+    }
+}
